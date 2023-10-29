d1d363eb407dddb8a0a35f40177f3a5e2a1e676f
Crystal Tucker <crystal.isabelle.tucker@gmail.com>
2023-10-29 17:29:36 -0500

    refa: Document vertices

--- Core/GParticleSystem/GParticleVertex.cs
+++ Core/GParticleSystem/GParticleVertex.cs
@@ -5,30 +5,67 @@ namespace ParticleLibrary.Core
 {
 	internal struct GParticleVertex : IVertexType
 	{
+		/// <summary>
+		/// Position of particle
+		/// </summary>
 		public Vector4 Position;
+
+		/// <summary>
+		/// Texture coordinate of this vertex
+		/// </summary>
 		public Vector2 TexCoord;
 
+		/// <summary>
+		/// Starting color
+		/// </summary>
 		public Color StartColor;
+
+		/// <summary>
+		/// Ending color
+		/// </summary>
 		public Color EndColor;
 
+		/// <summary>
+		/// Packed Velocity (XY), and Velocity Acceleration (ZW)
+		/// </summary>
 		public Vector4 Velocity;
+
+		/// <summary>
+		/// Texture width and height
+		/// </summary>
 		public Vector2 Size;
+
+		/// <summary>
+		/// Scale (XY), and Scale Velocity (ZW)
+		/// </summary>
 		public Vector4 Scale;
+
+		/// <summary>
+		/// Corner (XY), Rotation (Z), and Rotation Velocity (W)
+		/// </summary>
 		public Vector4 Rotation;
 
+		/// <summary>
+		/// Depth (X), Depth Velocity (Y), and Time (Z)
+		/// </summary>
 		public Vector3 DepthTime;
 
 		public static readonly VertexDeclaration VertexDeclaration = new
 		(
 			new VertexElement(sizeof(float) * 0, VertexElementFormat.Vector4, VertexElementUsage.Position, 0),
+
 			new VertexElement(sizeof(float) * 4, VertexElementFormat.Vector2, VertexElementUsage.TextureCoordinate, 0),
 
 			new VertexElement(sizeof(float) * 6, VertexElementFormat.Color, VertexElementUsage.Color, 0), // Start Color
+
 			new VertexElement(sizeof(float) * 7, VertexElementFormat.Color, VertexElementUsage.Color, 1), // End Color
 
 			new VertexElement(sizeof(float) * 8, VertexElementFormat.Vector4, VertexElementUsage.Normal, 0), // Velocity
+
 			new VertexElement(sizeof(float) * 12, VertexElementFormat.Vector2, VertexElementUsage.Normal, 1), // Size
+
 			new VertexElement(sizeof(float) * 14, VertexElementFormat.Vector4, VertexElementUsage.Normal, 2), // Scale
+
 			new VertexElement(sizeof(float) * 18, VertexElementFormat.Vector4, VertexElementUsage.Normal, 3), // Rotation
 
 			new VertexElement(sizeof(float) * 22, VertexElementFormat.Vector3, VertexElementUsage.Normal, 4) // Depth Time
--- Core/PointParticleSystem/PointParticleVertex.cs
+++ Core/PointParticleSystem/PointParticleVertex.cs
@@ -5,13 +5,29 @@ namespace ParticleLibrary.Core.PointParticleSystem
 {
 	internal struct PointParticleVertex : IVertexType
 	{
+		/// <summary>
+		/// Position of particle
+		/// </summary>
 		public Vector4 Position;
 
+		/// <summary>
+		/// Starting color
+		/// </summary>
 		public Color StartColor;
+
+		/// <summary>
+		/// Ending color
+		/// </summary>
 		public Color EndColor;
 
+		/// <summary>
+		/// Packed Velocity (XY), and Velocity Acceleration (ZW)
+		/// </summary>
 		public Vector4 Velocity;
 
+		/// <summary>
+		/// Depth (X), Depth Velocity (Y), and Time (Z)
+		/// </summary>
 		public Vector3 DepthTime;
 
 		public static readonly VertexDeclaration VertexDeclaration = new
@@ -19,6 +35,7 @@ namespace ParticleLibrary.Core.PointParticleSystem
 			new VertexElement(sizeof(float) * 0, VertexElementFormat.Vector4, VertexElementUsage.Position, 0),
 
 			new VertexElement(sizeof(float) * 4, VertexElementFormat.Color, VertexElementUsage.Color, 0), // Start Color
+
 			new VertexElement(sizeof(float) * 5, VertexElementFormat.Color, VertexElementUsage.Color, 1), // End Color
 
 			new VertexElement(sizeof(float) * 6, VertexElementFormat.Vector4, VertexElementUsage.Normal, 0), // Velocity
