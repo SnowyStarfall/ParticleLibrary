dcb507abee168421988ac612326c18f500d54661
Crystal Tucker <crystal.isabelle.tucker@gmail.com>
2023-11-08 18:25:21 -0600

    refa: List, add: ListItem

---
+++ UI/Elements/Base/List.cs
@@ -0,0 +1,166 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Terraria.UI;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Xml.Linq;
+using Terraria;
+using Terraria.GameContent.UI.Elements;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ParticleLibrary.UI.Elements.Base
+{
+	public class List : Panel
+	{
+		public ObservableCollection<UIElement> Items { get; }
+
+		public IReadOnlyCollection<UIElement> SelectedItems { get => _selectedItems.AsReadOnly(); }
+		private readonly List<UIElement> _selectedItems;
+
+		public StyleDimension ItemHeight { get; set; }
+		public StyleDimension ItemPadding { get; set; }
+
+		private InnerList _innerList;
+
+		public List(Color fill, Color outline, float outlineThickness = 1, float cornerRadius = 0, bool soft = false) : base(fill, outline, outlineThickness, cornerRadius, soft)
+		{
+			Items = new();
+			Items.CollectionChanged += Items_CollectionChanged;
+
+			ItemHeight = new StyleDimension(24f, 0f);
+			ItemPadding = new StyleDimension(4f, 0f);
+
+			_selectedItems = new();
+
+			_innerList = new();
+			_innerList.OverflowHidden = false;
+			_innerList.Width.Set(0f, 1f);
+			_innerList.Height.Set(0f, 1f);
+			Append(_innerList);
+
+			OverflowHidden = true;
+		}
+
+		private class InnerList : UIElement
+		{
+			public override bool ContainsPoint(Vector2 point) => true;
+
+			protected override void DrawChildren(SpriteBatch spriteBatch)
+			{
+				Rectangle parentDim = Parent.GetDimensions().ToRectangle();
+
+				foreach (UIElement element in Elements)
+				{
+					Rectangle elementDim = element.GetDimensions().ToRectangle();
+
+					if (parentDim.Contains(elementDim) || elementDim.Intersects(parentDim))
+						element.Draw(spriteBatch);
+				}
+			}
+
+			public override Rectangle GetViewCullingArea() => Parent.GetDimensions().ToRectangle();
+		}
+
+		public virtual void Add(UIElement item)
+		{
+			Items.Add(item);
+			_innerList.Append(item);
+			//UpdateOrder();
+			_innerList.Recalculate();
+		}
+
+		public virtual bool Remove(UIElement item)
+		{
+			_innerList.RemoveChild(item);
+			//UpdateOrder();
+			return Items.Remove(item);
+		}
+
+		public virtual void Clear()
+		{
+			_innerList.RemoveAllChildren();
+			Items.Clear();
+		}
+
+		public override void Recalculate()
+		{
+			
+			base.Recalculate();
+			//UpdateScrollbar();
+		}
+
+		public override void ScrollWheel(UIScrollWheelEvent evt)
+		{
+			base.ScrollWheel(evt);
+			//if (_scrollbar != null)
+			//	_scrollbar.ViewPosition -= evt.ScrollWheelValue;
+		}
+
+		public override void RecalculateChildren()
+		{
+			base.RecalculateChildren();
+
+			//CalculatedStyle inner = GetInnerDimensions();
+
+			//float height = ItemHeight.GetValue(inner.Height);
+			//float padding = ItemPadding.GetValue(inner.Height);
+			//float totalHeight = Items.Count * (height + padding);
+			//float exposableHeight = inner.Height / totalHeight;
+
+			//float top = 0f;
+
+			//foreach (var item in Items)
+			//{
+			//	item.Top.Set(top, 0f);
+			//	item.Width.Set(0f, 1f);
+			//	item.Height = ItemHeight;
+			//	item.Recalculate();
+			//	top += height + padding;
+			//}
+
+			//float num = 0f;
+			//for (int i = 0; i < _items.Count; i++)
+			//{
+			//	float num2 = ((_items.Count == 1) ? 0f : ListPadding);
+			//	_items[i].Top.Set(num, 0f);
+			//	_items[i].Recalculate();
+			//	num += _items[i].GetOuterDimensions().Height + num2;
+			//}
+
+			//_innerListHeight = num;
+		}
+
+		private void Items_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+		{
+			if (e.OldItems is not null)
+			{
+				foreach (UIElement item in e.OldItems)
+				{
+					item.OnLeftClick -= Item_OnClick;
+				}
+			}
+
+			if (e.NewItems is not null)
+			{
+				foreach (UIElement item in e.NewItems)
+				{
+					item.OnLeftClick += Item_OnClick;
+				}
+			}
+		}
+
+		private void Item_OnClick(UIMouseEvent evt, UIElement listeningElement)
+		{
+			if (!Items.Contains(evt.Target))
+				return;
+
+			if (_selectedItems.Contains(evt.Target))
+			{
+				_selectedItems.Remove(evt.Target);
+				return;
+			}
+
+			_selectedItems.Add(evt.Target);
+		}
+	}
+}
---
+++ UI/Elements/Base/ListItem.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ParticleLibrary.UI.Interfaces;
+using ParticleLibrary.Utilities;
+using ReLogic.Graphics;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.UI;
+
+namespace ParticleLibrary.UI.Elements.Base
+{
+	public class ListItem : UIElement, IConsistentUpdateable
+	{
+		public object Content { get; set; }
+
+		private bool _overflows;
+		private float _hiddenLength;
+
+		private int _startCounter;
+		private int _moveCounter;
+		private int _endCounter;
+
+		public ListItem(object content)
+		{
+			Content = content;
+			_overflows = Overflows();
+		}
+
+		public void Update()
+		{
+			if (_overflows)
+			{
+				if (_endCounter >= 60)
+				{
+					_startCounter = 0;
+					_moveCounter = 0;
+					_endCounter = 0;
+				}
+
+				if (_moveCounter >= 100 && _endCounter < 60)
+				{
+					_endCounter++;
+				}
+
+				if (_startCounter >= 60 && _moveCounter < 100)
+				{
+					_moveCounter++;
+				}
+
+				if (_startCounter < 60)
+				{
+					_startCounter++;
+				}
+
+				return;
+			}
+
+			_startCounter = 0;
+			_moveCounter = 0;
+			_endCounter = 0;
+		}
+
+		protected override void DrawSelf(SpriteBatch spriteBatch)
+		{
+			_overflows = Overflows();
+
+			CalculatedStyle dim = GetInnerDimensions();
+			Vector2 size = FontAssets.MouseText.Value.MeasureString(Content.ToString());
+			float scale = Utils.Clamp(dim.Height / size.Y, 0f, 2f);
+
+			spriteBatch.DrawText(Content.ToString(), 1, dim.Position() - new Vector2((_moveCounter / 100f) * _hiddenLength, 0f), Color.White, Color.Black, scale: scale);
+		}
+
+		private bool Overflows()
+		{
+			Vector2 position = GetInnerDimensions().Position();
+			Vector2 size = FontAssets.MouseText.Value.MeasureString(Content.ToString());
+
+			Rectangle dim = GetInnerDimensions().ToRectangle();
+			Rectangle textDim = new((int)position.X, (int)position.Y, (int)size.X, (int)size.Y);
+
+			if ((textDim.X + textDim.Width) > (dim.X + dim.Width))
+			{
+				_hiddenLength = (position.X + size.X) - (dim.X + dim.Width);
+				return true;
+			}
+
+			_hiddenLength = 0f;
+
+			return false;
+		}
+	}
+}
